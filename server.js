9529391b08dc7ebe8f503aa3a9071452900bad80
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-12-13 14:12:43 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ app.get("/greeting/:name?", (req, res) => {
     // getting the param object
     const paramObj = req.params
 
-    // getting the keys of the pram object
-    const keys = Object.keys(paramObj)
-    // console.log(paramObj)
-
     // if the paramObj has a value for its 'name' key
     if (paramObj['name']) {
         res.send(`Wow! Hello there, ${paramObj.name}`)
@@ -38,6 +34,23 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
 })
 
 
+//********************* 
+//MAGIC 8 BALL
+//*********************
+const magicResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get("/magic/:ques", (req, res) => {
+
+    // getting the 'ques' param value
+    const ques = req.params.ques
+
+    // generating random number to get a random Magic 8 response
+    const randomIndex = Math.floor(Math.random() * magicResponses.length)
+
+    res.send(`${ques}
+            <h1>${magicResponses[randomIndex]}</h1>`)
+
+})
 
 
 
