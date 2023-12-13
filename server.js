104198f5b96ddac6b5fcfe2850f035c2cd629e5b
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-12-13 13:50:37 -0500

    Greeting express application created.

---
+++ server.js
@@ -0,0 +1,35 @@
+// import express library
+const express = require("express")
+
+// create express app object
+const app = express()
+
+//********************* 
+//GREETINGS
+//*********************
+
+// :name? - the ? makes the param optional
+app.get("/greeting/:name?", (req, res) => {
+
+    // getting the param object
+    const paramObj = req.params
+
+    // getting the keys of the pram object
+    const keys = Object.keys(paramObj)
+    // console.log(paramObj)
+
+    // if the paramObj has a value for its 'name' key
+    if (paramObj['name']) {
+        res.send(`Wow! Hello there, ${paramObj.name}`)
+    }
+    else {
+        res.send("Hello, stranger")
+    }
+})
+
+
+
+
+// app listener
+app.listen(3000, () => {console.log('Server listening on port 3000')})
+
